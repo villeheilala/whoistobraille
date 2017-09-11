154f33279f68b3819241cbbbf9aefd8c908f47de
Ville Heilala <villeheilala@users.noreply.github.com>
2017-09-11 13:39:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, url_for, render_template
 from zeep import Client
 import base64
@@ -8,7 +7,6 @@ app = Flask(__name__)
 def toBraille(url):
     client = Client('http://www.webservicex.net/whois.asmx?WSDL')
     result = client.service.GetWhoIS(url)
-
     client = Client('http://www.webservicex.net/braille.asmx?WSDL')
     result = client.service.BrailleText(result, 12)
     return result
@@ -16,12 +14,11 @@ def toBraille(url):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        url = request.form["url"]
-        braille = toBraille(url)
-        braille = base64.b64encode(braille)
-        return render_template('index.html', braille=braille.decode("utf-8"))
+        url = request.form['url']
+        braille = base64.b64encode(toBraille(url))
+        return render_template('index.html', braille=braille.decode('utf-8'))
     else:
-        return render_template('index.html', braille="")
+        return render_template('index.html', braille='')
 
 if __name__ == '__main__':
     app.run(debug=True)
